ca14c1d6c05e3d370b1ec5de171cb4e69c152dfd
perfectsquare123 <91461666+perfectsquare123@users.noreply.github.com>
2023-05-11 13:01:44 +0800

    create tables

---
+++ Database/client.sql
@@ -0,0 +1,35 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+08:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `client`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client`
+--
+
+CREATE DATABASE IF NOT EXISTS `client` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `client`;
+
+DROP TABLE IF EXISTS `client`;
+
+DROP TABLE IF EXISTS `client`;
+CREATE TABLE IF NOT EXISTS `client` (
+  `clientID` varchar(20) NOT NULL,
+  `Commission` float NOT NULL,
+  `GrossAmount` float NOT NULL,
+  `lastUpdateTime` datetime NOT NULL,
+  PRIMARY KEY (`clientID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
